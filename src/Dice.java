b3232fec337fc7d369129324afa1c2a1b8d0c76e
2cansam <samuelhubbard@live.com>
2019-12-23 21:23:34 -0600

    re coded logic for dice

--- src/Dice.java
+++ src/Dice.java
@@ -1,38 +1,47 @@
+import java.sql.SQLOutput;
 import java.util.Random;
 
 public class Dice {
-   private int die1;
-   private int die2;
-   private int die3;
-   private int die4;
-   private int die5;
-   private String message = "You rolled " + die1 +"pandas " + die2 + "bamboo " + die3 +"droplets " + (die4 + die5) + " blanks";
+    private int die1;
+    private int die2;
+    private int die3;
+    private int die4;
+    private int die5;
+    //private String message = "You rolled " + die1 + "pandas "; //+ die2 + "bamboo " + die3 + "droplets " + (die4 + die5) + " blanks";
 
-   public Dice(){
-       roll();
-   }
-
-   public void roll(){
-       // random 5 dice
-       die1 = (int)(Math.random()*6);
-       die2 = (int)(Math.random()*6);
-       die3 = (int)(Math.random()*6);
-       die4 = (int)(Math.random()*6);
-       die5 = (int)(Math.random()*6);
-
-       // if sum of dice is even or odd print message
-        int sum = die1 + die2 + die3 + die4 + die5;
-        if(sum/2 == 0){
-            System.out.println(message);
-        } else  {
-            System.out.println(message);
-        }
-   }
-
-    public int getDie() {
-        return die1;
+    Random r = new Random();
+    public Dice() {
+        //roll();
     }
 
+    public void roll() {
+        // random 5 dice
+       // Random r = new Random();
+        die1 = r.nextInt(6);
+
+       die2 = r.nextInt(6);
+        die3 = r.nextInt(6);
+        die4 = r.nextInt(6);
+        die5 = r.nextInt(6);
+        System.out.println("You rolled " + die1 + "pandas " + die2 + "bamboo " + die3 + "droplets " + (die4 / die5) + " blanks");}}
+
+
+        // if sum of dice is even or odd print message
+       /* int sum = die1; //+ die2 + die3 + die4 + die5;
+        if (sum / 2 == 0) {
+            System.out.println(sum);
+        } else {
+            System.out.println(message);
+        }
+    }
+}
+
+   /* public int getDie() {
+        return die1;
+    }
+}
+
+    /*
     public int getDie2() {
         return die2;
     }
@@ -50,3 +59,4 @@ public class Dice {
     }
 
 }
+*/
--- src/Main_Game.java
+++ src/Main_Game.java
@@ -1,2 +1,6 @@
 public class Main_Game {
+    public static void main(String[] args) {
+        Dice r1 = new Dice();
+        r1.roll();
+    }
 }
